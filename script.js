e1289c8f2c88ebc6b109954384f8ac537c181c71
ericafk0001 <dxslyofficial@gmail.com>
2024-12-01 16:55:52 -0500

    Setup Game development update readme.md

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const mainMenu = document.getElementById("main-menu");
-  const singleplayerBtn = document.getElementById("singleplayer-btn");
-
-  function startGame() {
-    const fadeScreen = document.getElementById("fade-screen");
-    fadeScreen.style.display = "block";
-    setTimeout(() => {
-      fadeScreen.classList.add("fade-in");
-    }, 8);
-  }
-
-  singleplayerBtn.addEventListener("click", function () {
-    mainMenu.style.display = "none";
-    startGame();
-  });
+  // const mainMenu = document.getElementById("main-menu");
+  // const singleplayerBtn = document.getElementById("singleplayer-btn");
+  // function startGame() {
+  //   const fadeScreen = document.getElementById("fade-screen");
+  //   fadeScreen.style.display = "block";
+  //   setTimeout(() => {
+  //     fadeScreen.classList.add("fade-in");
+  //   }, 8);
+  // }
+  // singleplayerBtn.addEventListener("click", function () {
+  //   mainMenu.style.display = "none";
+  //   startGame();
+  // });
 });
