ca615a04c6864961909467cb4e2e2a0c21a7fb4a
ericafk0001 <dxslyofficial@gmail.com>
2024-12-01 16:47:58 -0500

    Add fading animation

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const mainMenu = document.getElementById("main-menu");
+  const singleplayerBtn = document.getElementById("singleplayer-btn");
+
+  function startGame() {
+    const fadeScreen = document.getElementById("fade-screen");
+    fadeScreen.style.display = "block";
+    setTimeout(() => {
+      fadeScreen.classList.add("fade-in");
+    }, 8);
+  }
+
+  singleplayerBtn.addEventListener("click", function () {
+    mainMenu.style.display = "none";
+    startGame();
+  });
+});
